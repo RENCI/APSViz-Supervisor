5f5bb98da40c52d286cd2a50e61ba20ccc9e4024
Phil Owen <phillipsowen@outlook.com>
2021-10-01 13:44:14 -0400

    sending a less context on run handler errors

--- supervisor/src/job_supervisor.py
+++ supervisor/src/job_supervisor.py
@@ -236,9 +236,7 @@ class APSVizSupervisor:
                     no_activity = self.handle_run(run)
                 except Exception as e:
                     # report the exception
-                    self.logger.exception(f"Run handler exception detected, id: {run['id']}, \
-                                        job-type: {run['job-type']}, status: {run['status_prov']}, downloadurl: {run['downloadurl']}, \
-                                        gridname: {run['gridname']}, instance_name: {run['instance_name']}, exception: {e}")
+                    self.logger.exception(f"Run handler exception detected, id: {run['id']}, exception: {e}")
 
                     # prepare the DB status
                     run['status_prov'] += ', Run handler error detected'
