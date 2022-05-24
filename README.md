28732e5423092ec43ea4a5bebcb92a35b5374a09
Phil Owen <phillipsowen@outlook.com>
2022-05-24 13:30:11 -0400

    adding licensing information

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+<!--
+SPDX-FileCopyrightText: 2022 Phillips Owen <powen@renci.org>
+
+SPDX-License-Identifier: MIT
+-->
+
 # APSVIZ-k8s
 K8s functionalities
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Phillips Owen <powen@renci.org>
+#
+# SPDX-License-Identifier: MIT
+
 from supervisor.src.job_supervisor import APSVizSupervisor
 
 # create the supervisor
