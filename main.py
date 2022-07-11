09be7350eb4875975981ec88c1ae12f0dbff3342
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-07-11 11:43:56 -0400

    adding licensing info

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: 2022 Phillips Owen <powen@renci.org>
+# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
 #
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
 # SPDX-License-Identifier: MIT
 
 from supervisor.src.job_supervisor import APSVizSupervisor
