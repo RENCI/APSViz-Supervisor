9b37907f90fee0f99bdc7c122444b59cc4a2a045
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-07-13 12:41:56 -0400

    making sure that a https url is used to access thredds

--- supervisor/src/job_supervisor.py
+++ supervisor/src/job_supervisor.py
@@ -457,8 +457,8 @@ class APSVizSupervisor:
 
         # if there is a special k8s download url in the data use it.
         if 'post.opendap.renci_tds-k8.downloadurl' in run_info:
-            # use only the service name and save it for the run
-            run_info['downloadurl'] = run_info['post.opendap.renci_tds-k8.downloadurl']
+            # use only the service name and save it for the run. also forcing the apviz thredds url to be https:
+            run_info['downloadurl'] = run_info['post.opendap.renci_tds-k8.downloadurl'].replace('http://apsviz-thredds', 'https://apsviz-thredds')
 
         # loop through the params and return the ones that are missing
         return f"{', '.join([run_param for run_param in self.required_run_params if run_param not in run_info])}", instance_name, debug_mode
