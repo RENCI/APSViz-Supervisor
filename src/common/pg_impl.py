f20beefd835b6b8ab3b8a42e1fa8367994486dd0
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-10-27 17:00:23 -0400

    correcting db env params

--- src/common/pg_impl.py
+++ src/common/pg_impl.py
@@ -58,7 +58,7 @@ class PGImplementation(PGUtilsMultiConnect):
         sql: str = 'SELECT public.get_supervisor_job_defs_json()'
 
         # get the data
-        ret_val = self.exec_sql('irods-k8s', sql)
+        ret_val = self.exec_sql('irods-sv', sql)
 
         # return the data
         return ret_val
@@ -74,7 +74,7 @@ class PGImplementation(PGUtilsMultiConnect):
         sql: str = 'SELECT public.get_supervisor_config_items_json()'
 
         # get the data
-        ret_val = self.exec_sql('irods-k8s', sql)
+        ret_val = self.exec_sql('irods-sv', sql)
 
         # if there were no runs return None
         if ret_val == -1:
@@ -102,11 +102,11 @@ class PGImplementation(PGUtilsMultiConnect):
         sql = f"SELECT public.set_config_item({int(run[0])}, '{uid}', 'supervisor_job_status', '{value[:1024]}')"
 
         # run the SQL
-        ret_val = self.exec_sql('irods-k8s', sql)
+        ret_val = self.exec_sql('irods-sv', sql)
 
         # if there were no errors, commit the updates
         if ret_val > -1:
-            self.commit('irods-k8s')
+            self.commit('irods-sv')
 
     def get_first_job(self, workflow_type: str):
         """
@@ -118,7 +118,7 @@ class PGImplementation(PGUtilsMultiConnect):
         sql: str = f"SELECT public.get_supervisor_job_order('{workflow_type}')"
 
         # get the order of jobs for this workflow type
-        jobs_in_order = self.exec_sql('irods-k8s', sql)
+        jobs_in_order = self.exec_sql('irods-sv', sql)
 
         # if we got a list get the first one
         if isinstance(jobs_in_order, list):
